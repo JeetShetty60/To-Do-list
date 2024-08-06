84abe5a93347f2961dbc4761eb7af0f407860137
Jeet Shetty <161433012+JeetShetty60@users.noreply.github.com>
2024-08-06 12:43:31 +0530

    navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,6 @@ const Navbar = () => {
   return (
    <>
    <nav className='flex justify-around h-[15vh] items-center bg-violet-500 rounded-b-xl'>
-    <h2 className='font-bold text-2xl cursor-pointer'>Logo</h2>
     <ul className='flex gap-10'>
     <Link href="/" className='text-white'>Home</Link>
     <Link href="/Features" className='text-white'>Features</Link>
